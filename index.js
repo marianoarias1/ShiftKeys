86b5a24701294bdb40c43ca44152fcac73060a9f
Mariano Arias <marianoarias123@hotmail.com>
2022-05-01 15:07:35 -0300

    pantalla de carga

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+window.onload= function(){
+    $(`#onload`).fadeOut();
+    $(`body`).removeClass('hidden')
+}
+
 /* Animacion maquina de escribir */
 
 let app = document.getElementById('titulo_nosotros');
@@ -13,19 +18,21 @@ typewriter.typeString('ShiftKeys')
 
 /* Slider */
 
+
+
 window.addEventListener("load", ()=>{
     let imagenes= [];
-
     imagenes[0]="./Slider/slider1.jpg"
     imagenes[1]="./Slider/slider2.jpg"
     imagenes[2]="./Slider/slider3.jpg"
+    imagenes[3]="./Slider/slider4.jpg"
 
     let indiceImagenes=0;
 
     function cambiarImagenes (){
         document.slider.src=imagenes[indiceImagenes];
 
-        if(indiceImagenes < 2){
+        if(indiceImagenes < 3){
             indiceImagenes++;
         }else{
             indiceImagenes=0;
