f301aef5bcaaf57355c2da3532b4acd59ab1faef
Mariano Arias <marianoarias123@hotmail.com>
2022-05-01 02:54:46 -0300

    slider

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-let app = document.getElementById('titulo_header');
+/* Animacion maquina de escribir */
+
+let app = document.getElementById('titulo_nosotros');
 
 let typewriter = new Typewriter(app, {
     loop: false
@@ -7,3 +9,28 @@ let typewriter = new Typewriter(app, {
 typewriter.typeString('ShiftKeys')
     .pauseFor(2500)
     .start();
+
+
+/* Slider */
+
+window.addEventListener("load", ()=>{
+    let imagenes= [];
+
+    imagenes[0]="./Slider/slider1.jpg"
+    imagenes[1]="./Slider/slider2.jpg"
+    imagenes[2]="./Slider/slider3.jpg"
+
+    let indiceImagenes=0;
+
+    function cambiarImagenes (){
+        document.slider.src=imagenes[indiceImagenes];
+
+        if(indiceImagenes < 2){
+            indiceImagenes++;
+        }else{
+            indiceImagenes=0;
+        }
+    }
+
+    setInterval(cambiarImagenes,2000)
+})
